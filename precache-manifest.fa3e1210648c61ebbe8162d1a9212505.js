751348edf9c16a8ebc41d674b371a3e430a53b18
nguyenntt <nguyen.nguyen@xoontec.com>
2019-07-12 10:49:47 +0700

    Updates

---
+++ precache-manifest.fa3e1210648c61ebbe8162d1a9212505.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4792b503f0cbfdc4328a7e44d69226f5",
+    "url": "/web-react-todo-hook/index.html"
+  },
+  {
+    "revision": "a8e35271136a3eda239e",
+    "url": "/web-react-todo-hook/static/css/3.80f9a7c9.chunk.css"
+  },
+  {
+    "revision": "fda77575a4448ac70681",
+    "url": "/web-react-todo-hook/static/css/main.6a322e1f.chunk.css"
+  },
+  {
+    "revision": "ae5b62ea5bd9f4135c6c",
+    "url": "/web-react-todo-hook/static/js/2.0fa7bc3b.chunk.js"
+  },
+  {
+    "revision": "a8e35271136a3eda239e",
+    "url": "/web-react-todo-hook/static/js/3.da12c82d.chunk.js"
+  },
+  {
+    "revision": "eb1ab4e5a98a29e4739f",
+    "url": "/web-react-todo-hook/static/js/4.67a1a863.chunk.js"
+  },
+  {
+    "revision": "fda77575a4448ac70681",
+    "url": "/web-react-todo-hook/static/js/main.3e79f2fe.chunk.js"
+  },
+  {
+    "revision": "fad6c50c619c40868e03",
+    "url": "/web-react-todo-hook/static/js/runtime~main.967d1b3d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todo-app/precache-manifest.ba4af87ac4c85b0b679017275de9bc9a.js"
+  "/web-react-todo-hook/precache-manifest.fa3e1210648c61ebbe8162d1a9212505.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/web-react-todo-hook/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
